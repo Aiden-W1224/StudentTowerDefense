4e440eb93d47c8d634a85922c0c58195a4cb6d71
FreeKodak <37547012+FreeKodak@users.noreply.github.com>
2018-03-26 12:34:29 -0600

    Add files via upload

--- Tower_place.java
+++ Tower_place.java
@@ -10,29 +10,30 @@ public class Tower_place
 	
 	public Tower_place() {}
 	
-	public int[][] place_towers(int[][] map, Level_generator gen)
+	public int[][] place_towers(int[][] map, Level_generator gen, int x_coord, int y_coord, int tower_num)
 	{
-		this.x_coord = 0;
-		this.y_coord = 0;
-		//while ((x_coord != -1) && (y_coord != -1))
-		//{
-			System.out.println("Select co-ordinates for a tower.");
-			System.out.println("x co-ordinate: ");
-			this.x_coord = sc.nextInt();
-			System.out.println("y co-ordinate: ");
-			this.y_coord = sc.nextInt();
-			System.out.println("Type in tower type (First-year = 7, Second-year = 8): ");
-			this.tower_type = sc.nextInt();
+		//this.x_coord = 0;
+		//this.y_coord = 0;
+		
+//			System.out.println("Select co-ordinates for a tower.");
+//			System.out.println("x co-ordinate: ");
+//			this.x_coord = sc.nextInt();
+//			System.out.println("y co-ordinate: ");
+//			this.y_coord = sc.nextInt();
+//			System.out.println("Type in tower type (First-year = 7, Second-year = 8): ");
+			this.tower_type = tower_num;
+		
+			
 			if (x_coord >= 0 && x_coord < map[0].length && y_coord >= 0 && y_coord < map.length)
 			{
-				Tower floof = new First_year(x_coord, y_coord, map);
+				Tower firstYear = new First_year(x_coord, y_coord, map);
 				switch (tower_type) 
 				{
 				case 7:
-					floof = new First_year(x_coord, y_coord, map);
+					firstYear = new First_year(x_coord, y_coord, map);
 					break;
 				case 8:
-					floof = new Second_year(x_coord, y_coord, map);
+					firstYear = new Second_year(x_coord, y_coord, map);
 					break;
 				}
 				/**System.out.println("Tower bounds: ");
@@ -41,8 +42,8 @@ public class Tower_place
 					System.out.print(Arrays.toString(floof.get_bounds().get(i)));
 				}
 				System.out.println("\n");*/
-				gen.add_to_arsenal(floof);
-				map[floof.get_y()][floof.get_x()] = floof.get_appearance();
+				gen.add_to_arsenal(firstYear);
+				map[firstYear.get_y()][firstYear.get_x()] = firstYear.get_appearance();
 				System.out.println("Map now: ");
 				for (int i = 0; i < map.length; i++) 
 				{
