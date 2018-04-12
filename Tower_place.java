1d87bbc70d0e1a1ab6c0184fa9075fa8456881d6
LingLing18 <37598704+LingLing18@users.noreply.github.com>
2018-04-12 17:21:23 -0600

    Update Tower_place.java

--- Tower_place.java
+++ Tower_place.java
@@ -14,35 +14,54 @@ public class Tower_place
 	{
 
 			this.tower_type = tower_num;
-		
-			
-			if (x_coord >= 0 && x_coord < map[0].length && y_coord >= 0 && y_coord < map.length)
-			{
-				Tower firstYear = new First_year(x_coord, y_coord, map);
-				switch (tower_type) 
-				{
-				case 7:
-					firstYear = new First_year(x_coord, y_coord, map);
-					player.setTuition(player.getTuition() - 250);
-					render.update_map(player);
-					break;
-				case 8:
-					firstYear = new Second_year(x_coord, y_coord, map);
-					player.setTuition(player.getTuition() - 500);
-					render.update_map(player);
-					break;
-				}
-				gen.add_to_arsenal(firstYear);
-				map[firstYear.get_y()][firstYear.get_x()] = firstYear.get_appearance();
-				System.out.println("Map now: ");
-				for (int i = 0; i < map.length; i++) 
-				{
-					System.out.println(Arrays.toString(map[i]));
-				}
-				System.out.println();
+			if (player.getTuition() < 250) {
+					System.out.println("You do not have enough money!");
 			}
-		
-		return map;
+			else if (player.getTuition() > 250 && player.getTuition() < 500) {
+				if (x_coord >= 0 && x_coord < map[0].length && y_coord >= 0 && y_coord < map.length)
+				{
+					Tower firstYear = new First_year(x_coord, y_coord, map);
+					switch (tower_type) 
+					{
+					case 7:
+						firstYear = new First_year(x_coord, y_coord, map);
+						player.setTuition(player.getTuition() - 250);
+						render.update_map(player);
+						break;
+					}
+				}
+				return map;
+			}
+			else {
+				if (x_coord >= 0 && x_coord < map[0].length && y_coord >= 0 && y_coord < map.length)
+				{
+					Tower firstYear = new First_year(x_coord, y_coord, map);
+					switch (tower_type) 
+					{
+					case 7:
+						firstYear = new First_year(x_coord, y_coord, map);
+						player.setTuition(player.getTuition() - 250);
+						render.update_map(player);
+						break;
+					case 8:
+						firstYear = new Second_year(x_coord, y_coord, map);
+						player.setTuition(player.getTuition() - 500);
+						render.update_map(player);
+						break;
+					}
+					gen.add_to_arsenal(firstYear);
+					map[firstYear.get_y()][firstYear.get_x()] = firstYear.get_appearance();
+					System.out.println("Map now: ");
+					for (int i = 0; i < map.length; i++) 
+					{
+						System.out.println(Arrays.toString(map[i]));
+					}
+					System.out.println();
+				}
+				return map;
+			}
+			return map;
+			
 	}
 	
 	public int get_x() 
